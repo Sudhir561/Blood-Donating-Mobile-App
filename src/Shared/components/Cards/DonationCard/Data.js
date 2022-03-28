31ea16d442543e3dd139f017da1ad81bec18cdc2
sudhir561 <sudhirme101@gmail.com>
2022-03-29 00:25:43 +0530

    Donoationrequest-screen added

---
+++ src/Shared/components/Cards/DonationCard/Data.js
@@ -0,0 +1,58 @@
+export const donationRequest = [
+    {
+        id:1,
+        name:"Sudhir ",
+        location:"Ranchi",
+        time:"5 Min Ago",
+
+    },
+{
+        id:2,
+        name:"Rashmi",
+        location:"Patna",
+        time:"13 Min Ago",
+
+    },
+{
+        id:3,
+        name:"Satish",
+        location:"Varanashi",
+        time:"20 Min Ago",
+
+    },
+{
+        id:4,
+        name:"Srinivash",
+        location:"Noida",
+        time:"50 Min Ago",
+
+    },
+    {
+        id:5,
+        name:"Srinivash",
+        location:"Noida",
+        time:"50 Min Ago",
+
+    },
+    {
+        id:6,
+        name:"Srikant",
+        location:"Andhra",
+        time:"59 Min Ago",
+
+    },
+    {
+        id:7,
+        name:"Sravani",
+        location:"Telengna",
+        time:"1hr Ago",
+
+    },
+    {
+        id:8,
+        name:"Vaibhav",
+        location:"M.P",
+        time:"1:30 Hr Ago",
+
+    },
+]
--- src/Shared/components/Cards/DonationCard/index.js
+++ src/Shared/components/Cards/DonationCard/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
-import colorValue from '../../Shared/commoStyle/ColorValue'
-import { commonJustify, commonStyle } from '../../Shared/commoStyle/CommonStyle'
-import fontValue from '../../Shared/commoStyle/FontValue'
+import colorValue from '../../../commoStyle/ColorValue'
+import { commonJustify, commonStyle } from '../../../commoStyle/CommonStyle'
+import fontValue from '../../../commoStyle/FontValue'
 
-const DonationComponent = ({item}) => {
+
+
+const DonationCard = ({item}) => {
+    <Image style={{width:100,height:100 }}  source={require("../../../../../assets/image/soal.png")}/>
     
     return (
         <View style={[styles.main, commonJustify.rowSpaceBetween]}>
+           
             
            <View>
            <View style={[styles.margin]}>
             <Text style={commonStyle({fontSize:13, fontFamily:fontValue.PoppinsRegular}).text}>Name</Text>
             <Text style={commonStyle({fontSize:18, fontFamily:fontValue.PoppinsExtraBold,color:colorValue.black}).text}>{item.name}</Text>
+            
             </View>
 
             <View style={[styles.margin]}>
@@ -28,7 +33,7 @@ const DonationComponent = ({item}) => {
            </View>
 
            <View  style={{paddingTop:20}} >
-               <Image style={{width:100, }} resizeMode="contain" source={require("../../../assets/image/BloodGroup.png")}/>
+           <Image style={{width:100, }} resizeMode="contain" source={require("../../../../../assets/image/BloodGroup.png")}/>
                <Button   titleStyle={{color:colorValue.primary}} type="clear" title="Donate" />
            </View>
 
@@ -37,7 +42,7 @@ const DonationComponent = ({item}) => {
     )
 }
 
-export default DonationComponent
+export default DonationCard
 
 const styles = StyleSheet.create({
     main:{
