8adfd07f7cf8e40ffbfd3401afafca679027028b
sudhir561 <sudhirme101@gmail.com>
2022-03-28 16:28:54 +0530

    adding login,register,homepage

---
+++ src/Screens/HomeScreen/Data.js
@@ -0,0 +1,70 @@
+export const ItemArray = [
+    {
+      id: 1,
+      title: 'Find Donners',
+      image: require('../../../assets/image/ion_search-outline.png'),
+    },
+    {
+      id: 2,
+      title: 'Donates',
+      image: require('../../../assets/image/openmoji_blood-transfusion.png'),
+    },
+    {
+      id: 3,
+      title: 'Order Bloods',
+      image: require('../../../assets/image/si-glyph_blood-bag.png'),
+    },
+    {
+      id: 4,
+      title: 'Assistant',
+      image: require('../../../assets/image/maki_doctor-11.png'),
+    },
+    {
+      id: 5,
+      title: 'Report',
+      image: require('../../../assets/image/la_file-medical-alt.png'),
+    },
+    {
+      id: 6,
+      title: 'Campains',
+      image: require('../../../assets/image/grommet-icons_announce.png'),
+    },
+  ];
+  
+  
+  
+  
+  
+  export const donationRequest = [
+      {
+          id:1,
+          name:"Manish ",
+          location:"Ranchi",
+          time:"5 Min Ago",
+  
+      },
+  {
+          id:2,
+          name:"Rashmi",
+          location:"Patna",
+          time:"13 Min Ago",
+  
+      },
+  {
+          id:3,
+          name:"Satish",
+          location:"Varanashi",
+          time:"20 Min Ago",
+  
+      },
+  {
+          id:4,
+          name:"Srinivash",
+          location:"Noida",
+          time:"50 Min Ago",
+  
+      },
+  
+  
+  
+  ]
---
+++ src/Screens/SearchScreen/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const SearchScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>SearchScreen</Text>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'purple',
+    },
+});
+
+//make this component available to the app
+export default SearchScreen;
